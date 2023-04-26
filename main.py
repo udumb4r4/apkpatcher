024afce327d7a1eb18c98cf28a2e3bd9fecb0f2f
Ariel Tubul <avarti765@gmail.com>
2023-04-26 10:30:28 +0300

    api decl

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import subprocess
 from typing import List
 from pathlib import Path
 import xml.etree.ElementTree as ET
+from frida_gadget import FridaGadget
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 
@@ -90,9 +91,9 @@ def parse_arguments():
     parser.add_argument('-a', '--apk', required=True, help='apk to patch')
     parser.add_argument('-g', '--gadget', required=False, help='frida-gadget file')
     parser.add_argument('-o', '--output-file', required=True, help='output patched apk')
-    parser.add_argument('-s', '--script-path', required=True, help='js script to inject')
+    parser.add_argument('-s', '--script', required=True, help='js script to inject')
     parser.add_argument('-i', '--install', action='store_true', help='adb install after build')
-    parser.add_argument('-k', '--keystore-path', required=False, help='path of keystore to use')
+    parser.add_argument('-k', '--keystore', required=False, help='path of keystore to use')
     parser.add_argument('-u', '--update-gadgets', action='store_true', help='download newest gadgets from github')
     parser.add_argument('-w', '--wait-before-repackage', action='store_true', help='Waits for your OK before repackaging the apk')
 
@@ -114,8 +115,8 @@ def main():
         with open(manifest_path, 'w') as manifest_file:
             manifest_file.write(manifest_xml)
 
-        # TODO: obtain frida-gadget files (import `frida-gadget`)
-        # TODO: create lib/<arch>/ folders with gadget and scripts
+        gadget = FridaGadget(args.update_gadgets, args.gadget) # TODO: obtain frida-gadget files (import `frida-gadget`)
+        gadget.add_gadget_libs(smali_folder, Path(args.script)) # TODO: create lib/<arch>/ folders with gadget and scripts
         # TODO: app-entry-point inject LoadLibrary
 
         if args.wait_before_repackage:
