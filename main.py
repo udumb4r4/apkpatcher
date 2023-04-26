955e8e51a8d96506986e868bad895b6cdbf93c8f
Ariel Tubul <avarti765@gmail.com>
2023-04-26 20:55:13 +0300

    updated main script skel, removed unused imports

--- main.py
+++ main.py
@@ -1,91 +1,26 @@
-import os
-import json
-import lzma
-import time
-import shutil
 import argparse
-import requests
 import buildapp
 import subprocess
-from typing import List
 from pathlib import Path
-import xml.etree.ElementTree as ET
 from frida_gadget import FridaGadget
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
-
-
-NATIVE_PERMISSION = 'android.app.extractNativeLibs'
-INTERNET_PERMISSION = 'android.permission.INTERNET'
+from android_manifest import AndroidManifest
 
 
 @contextmanager
 def decompiled_context(apk_path: Path):
     with TemporaryDirectory() as decompiled_path:
-        # TODO: f'apktool d {apk_path.resolve()} -o {decompiled_path}'
+        subprocess.run(
+            f'apktool d {apk_path.resolve()} -o {decompiled_path}',
+            shell=True,
+            check=True,
+            input='\n'.encode(),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.DEVNULL
+        )
+
         yield Path(decompiled_path)
-
-
-def add_internet_permission(manifest_xml: str) -> str: # TODO: test & refactor
-    root = ET.fromstring(manifest_xml)
-
-    internet_perm = None
-    for elem in root.iter('uses-permission'):
-        if elem.attrib.get('name') == INTERNET_PERMISSION:
-            internet_perm = elem
-            break
-
-    if internet_perm is None:
-        internet_perm = ET.Element('uses-permission', {'name': INTERNET_PERMISSION})
-        root.insert(0, internet_perm)
-
-    return ET.tostring(root, encoding='utf-8', method='xml').decode('utf-8')
-
-
-def allow_native_libs_extraction(manifest_xml: str) -> str:  # TODO: test & refactor
-    root = ET.fromstring(manifest_xml)
-    application_elem = root.find('application')
-
-    if application_elem is None:
-        application_elem = ET.Element('application')
-        root.append(application_elem)
-
-    extract_native_libs_meta = None
-    for elem in application_elem.iter('meta-data'):
-        if elem.attrib.get('name') == NATIVE_PERMISSION:
-            extract_native_libs_meta = elem
-            break
-
-    if extract_native_libs_meta is None:
-        extract_native_libs_meta = ET.Element('meta-data', {'name': NATIVE_PERMISSION, 'value': 'true'})
-        application_elem.append(extract_native_libs_meta)
-    else:
-        extract_native_libs_meta.attrib['value'] = 'true'
-
-    return ET.tostring(root, encoding='utf-8', method='xml').decode('utf-8')
-
-
-def find_app_entry_point(manifest_file): # TODO: test & refactor
-    tree = ET.parse(manifest_file)
-    root = tree.getroot()
-
-    activity_elem = None
-    for elem in root.iter('activity'):
-        intent_filter = elem.find('intent-filter')
-
-        if intent_filter is not None:
-            action_elem = intent_filter.find('action')
-            category_elem = intent_filter.find('category')
-
-            if (not action_elem) or (not category_elem):
-                continue
-
-            if (action_elem.attrib.get('name') == 'android.intent.action.MAIN') and (category_elem.attrib.get('name') == 'android.intent.category.LAUNCHER'):
-                activity_elem = elem
-                break
-
-    if activity_elem:
-        return activity_elem.attrib.get('name')
 
 
 def parse_arguments():
@@ -106,20 +41,16 @@ def main():
     args = parse_arguments()
 
     with decompiled_context(args.apk) as smali_folder:
-        manifest_path = smali_folder / 'AndroidManifest.xml'
-
-        with open(manifest_path, 'r') as manifest_file:
-            manifest_xml = manifest_file.read()
-
-        manifest_xml = add_internet_permission(manifest_xml)
-        manifest_xml = allow_native_libs_extraction(manifest_xml)
-
-        with open(manifest_path, 'w') as manifest_file:
-            manifest_file.write(manifest_xml)
-
         gadget = FridaGadget(args.update_gadgets, args.gadget)
         gadget.add_gadget_libs(smali_folder, Path(args.script))
 
+        manifest_path = smali_folder / 'AndroidManifest.xml'
+        manifest = AndroidManifest(manifest_path)
+
+        manifest.add_internet_permission()
+        manifest.allow_native_libs_extraction()
+
+        entry_point = manifest.find_app_entry_point()
         # TODO: app-entry-point inject LoadLibrary
 
         if args.wait_before_repackage:
