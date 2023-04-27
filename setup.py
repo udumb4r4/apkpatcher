579d5cef31e330433b8efc0a501f8c699e807ca6
Ariel Tubul <avarti765@gmail.com>
2023-04-27 21:33:09 +0300

    use of hard-coded requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
-from pathlib import Path
 
-
-REQUIREMENTS_FILE = Path('./requirements.txt')
 
 setuptools.setup(
     name = "apkmod",
@@ -11,6 +8,6 @@ setuptools.setup(
     description = "Apk frida-gadget injector script",
     packages = setuptools.find_packages(),
     url = "https://github.com/mon231/apkpatcher/",
-    install_requires = [req for req in REQUIREMENTS_FILE.read_text().splitlines() if req],
+    install_requires = ['requests', 'buildapp'],
     entry_points = {'console_scripts': ['apkmod=apkmod.main:main']}
 )
