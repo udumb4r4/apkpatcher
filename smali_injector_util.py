5806f6f799d9e8b71346d5dca08f74006f6cb8ca
Ariel Tubul <avarti765@gmail.com>
2023-04-27 11:54:56 +0300

    WIP impl injector util, implemented for non-static-ctor case

---
+++ smali_injector_util.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+
+class ActivitySmaliInjector:
+    def __init__(self, sources_path: Path, activity_name: str):
+        self.__activity_file = ActivitySmaliInjector.__find_activity_source_file(sources_path, activity_name)
+        activity_content = self.__activity_file.read_text()
+
+        self.__direct_methods_end = activity_content.find('# virtual methods')
+        self.__direct_methods_start = activity_content.find('# direct methods')
+
+        if (self.__direct_methods_start == -1) or (self.__direct_methods_end == -1):
+            raise Exception('Couldn\'t find direct & virtual methods sections')
+
+    def inject_to_activity(self, code_to_inject: str):
+        activity_content = self.__activity_file.read_text()
+
+        class_constructor_start_index = activity_content.find(
+            '.method static constructor <clinit>()V',
+            self.__direct_methods_start,
+            self.__direct_methods_end
+        )
+
+        if class_constructor_start_index == -1:
+            self.__patch_activity_without_class_constructor(activity_content, code_to_inject)
+        else:
+            self.__patch_activity_with_class_constructor(activity_content, code_to_inject)
+
+    def __patch_activity_without_class_constructor(self, injected_code: str):
+        main_activity_loader = '''
+        .method static constructor <clinit>()V
+            .locals 1
+            .prologue
+            {0}
+            return-void
+        .end method'''.format(injected_code)
+
+        activity_content = self.__activity_file.read_text()
+        loader_begin_index = self.__direct_methods_start + len('# direct methods') + 1
+
+        new_content = activity_content[0:loader_begin_index]
+        new_content += main_activity_loader
+        new_content += activity_content[loader_begin_index:]
+
+        self.__activity_file.write_text(new_content)
+
+    def __patch_activity_with_class_constructor(self, injected_code: str):
+        activity_content = self.__activity_file.read_text()
+
+        class_constructor_start_index = activity_content.find(
+            '.method static constructor <clinit>()V',
+            self.__direct_methods_start,
+            self.__direct_methods_end
+        )
+
+        class_constructor_end_index = activity_content.find(
+            '.end method',
+            class_constructor_start_index,
+            self.__direct_methods_end
+        )
+
+        if class_constructor_end_index == -1:
+            raise Exception('Error unexpected class constructor end')
+
+        class_constructor_prologue_index = activity_content.find(
+            '.prologue',
+            class_constructor_start_index,
+            class_constructor_end_index
+        )
+
+        if class_constructor_prologue_index == -1:
+            no_prologue_case = True
+
+            locals_start_index = activity_content.find('.locals ',
+                                                class_constructor_start_index, class_constructor_end_index)
+
+        if no_prologue_case and locals_start_index == -1:
+            self.print_warn('Has class constructor. No prologue case, but no "locals 0" found.')
+            return False
+
+        locals_end_index = -1
+        if no_prologue_case:
+            locals_end_index = locals_start_index + len('locals X')
+
+        prologue_end_index = -1
+        if has_class_constructor and class_constructor_prologue_index > -1:
+            prologue_end_index = class_constructor_prologue_index + len('.prologue') + 1
+
+        if no_prologue_case:
+            new_content = activity_content[0:locals_end_index]
+
+            if activity_content[locals_end_index] == '0':
+                new_content += '1'
+            else:
+                new_content += activity_content[locals_end_index]
+
+            new_content += '\n\n    .prologue'
+            new_content += load_library_injection_code
+            new_content += activity_content[locals_end_index+1:]
+
+            self.__activity_file.write_text(new_content)
+        else:
+            new_content = activity_content[0:prologue_end_index]
+            new_content += load_library_injection_code
+            new_content += activity_content[prologue_end_index:]
+            self.__activity_file.write_text(new_content)
+
+    @staticmethod
+    def __find_activity_source_file(sources_path: Path, activity_name: str) -> Path:
+        for smali_folder in sources_path.glob('**/smali*'):
+            activity_path = smali_folder / (activity_name.replace('.', '/') + '.smali')
+
+            if activity_path.is_file():
+                return activity_path
+
+        raise Exception(f'Activity smali file was not found')
