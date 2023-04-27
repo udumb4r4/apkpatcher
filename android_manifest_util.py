e17bde3e469444009b1e0903bcf1c6e1cf1942d4
Ariel Tubul <avarti765@gmail.com>
2023-04-27 10:47:30 +0300

    implemented entry-point getter

--- android_manifest_util.py
+++ android_manifest_util.py
@@ -5,7 +5,9 @@ import xml.etree.ElementTree as ET
 class AndroidManifestPatcher:
     def __init__(self, manifest_path: Path):
         self.__manifest_path = manifest_path
-        ET.register_namespace('android', AndroidManifestPatcher.__find_android_schema(manifest_path))
+        self.__android_schema = AndroidManifestPatcher.__find_android_schema(manifest_path)
+
+        ET.register_namespace('android', self.__android_schema)
 
     def allow_internet_permission(self):
         INTERNET_PERMISSION = 'android.permission.INTERNET'
@@ -15,47 +17,50 @@ class AndroidManifestPatcher:
             if INTERNET_PERMISSION in permission.attrib.values():
                 return
 
-        internet_perm = ET.Element('uses-permission', {f'android:name': INTERNET_PERMISSION})
+        internet_perm = ET.Element('uses-permission', {'android:name': INTERNET_PERMISSION})
         root.insert(0, internet_perm)
 
         self.__set_content(ET.tostring(root))
 
     def allow_native_libs_extraction(self):
-        NATIVE_PERMISSION = 'extractNativeLibs'
         root = ET.fromstring(self.__get_content())
-
         application_elem = root.find('application')
 
         if not application_elem:
             raise Exception('Manifest got no application tag')
 
+        NATIVE_PERMISSION = 'extractNativeLibs'
+        if application_elem.get(f'{{{self.__android_schema}}}{NATIVE_PERMISSION}') == 'true':
+            return
+
         application_elem.set(f'android:{NATIVE_PERMISSION}', 'true')
         self.__set_content(ET.tostring(root))
 
-    def find_app_entry_point(self) -> str: # TODO: test & refactor
+    def find_app_entry_point(self) -> str:
         root = ET.fromstring(self.__get_content())
+        application_elem = root.find('application')
 
-        activity_elem = None
-        for elem in root.iter('activity'):
-            intent_filter = elem.find('intent-filter')
+        if not application_elem:
+            raise Exception('Manifest got no application tag')
 
-            if intent_filter is not None:
+        for activity in application_elem.iter('activity'):
+            for intent_filter in activity.iter('intent-filter'):
                 action_elem = intent_filter.find('action')
                 category_elem = intent_filter.find('category')
+                android_name_tag = f'{{{self.__android_schema}}}name'
 
-                if (not action_elem) or (not category_elem):
+                if (action_elem is None) or (category_elem is None):
                     continue
 
-                if action_elem.attrib.get('name') != 'android.intent.action.MAIN':
+                if action_elem.get(android_name_tag) != 'android.intent.action.MAIN':
                     continue
 
-                if category_elem.attrib.get('name') != 'android.intent.category.LAUNCHER':
+                if category_elem.attrib.get(android_name_tag) != 'android.intent.category.LAUNCHER':
                     continue
 
-                activity_elem = elem
+                return activity.attrib.get(android_name_tag)
 
-        if activity_elem:
-            return activity_elem.attrib.get('name')
+        raise Exception('Couldn\'t find activity with MAIN action for LAUNCHER catagory')
 
     def __get_content(self) -> str:
         with open(self.__manifest_path, 'r') as manifest_file:
