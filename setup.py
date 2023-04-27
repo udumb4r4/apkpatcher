2deab174e4f22758cb567269bd122c9cf3240e0e
Ariel Tubul <avarti765@gmail.com>
2023-04-27 21:19:49 +0300

    Added requirements to pkg setup

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+from pathlib import Path
+
+
+REQUIREMENTS_FILE = Path('./requirements.txt')
 
 setuptools.setup(
     name = "apkmod",
@@ -7,7 +11,6 @@ setuptools.setup(
     description = "Apk frida-gadget injector script",
     packages = setuptools.find_packages(),
     url = "https://github.com/mon231/apkpatcher/",
-    entry_points = {
-        'console_scripts': ['apkmod=apkmod.main:main'],
-    }
+    install_requires = [req for req in REQUIREMENTS_FILE.read_text().splitlines() if req],
+    entry_points = {'console_scripts': ['apkmod=apkmod.main:main']}
 )
